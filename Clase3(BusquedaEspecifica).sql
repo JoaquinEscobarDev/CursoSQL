a6ed123d5d1ba113802301c4430c011eaa32722f
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-01-20 20:58:40 -0300

    query Clase 3, Busqueda especifica con AND, OR, IS NULL, IS NOT NULL

--- Clase3(BusquedaEspecifica).sql
+++ Clase3(BusquedaEspecifica).sql
@@ -1,18 +1,26 @@
---nombres que empiezan con la letra J
-SELECT * FROM STUDENTS WHERE FIRSTNAME LIKE 'J%';
+-- nombres que empiezan con la letra J
+-- SELECT * FROM STUDENTS WHERE FIRSTNAME LIKE 'J%';
 
---nombre que contenga la letra A
-SELECT * FROM STUDENTS WHERE FIRSTNAME LIKE '%A%';
+-- nombre que contenga la letra A
+-- SELECT * FROM STUDENTS WHERE FIRSTNAME LIKE '%A%';
 
---edades que sean 20
-SELECT * FROM STUDENTS WHERE AGE = 22;
+ --edades que sean mayores a 25
+ SELECT * FROM STUDENTS WHERE AGE > 25;
 
---edades que esten entre 20 y 25
-SELECT * FROM STUDENTS WHERE AGE BETWEEN 20 AND 25;
-SELECT * FROM STUDENTS WHERE AGE >= 20 AND AGE <= 25;
+ --edades que sean menores o iguales a 18
+ SELECT * FROM STUDENTS WHERE AGE <= 18;
 
---apellido que contengan la O
-SELECT * FROM STUDENTS WHERE LASTNAME LIKE '%O%';
+ --apellido que contengan la letra E
+ SELECT * FROM STUDENTS WHERE LASTNAME LIKE '%E%';
 
---apellido que empieza con la B
-SELECT * FROM STUDENTS WHERE LASTNAME LIKE '%B%';
+ --apellido que empieza con la letra C
+ SELECT * FROM STUDENTS WHERE LASTNAME LIKE 'C%';
+
+ --estudiantes cuyo nombre no es nulo
+ SELECT * FROM STUDENTS WHERE FIRSTNAME IS NOT NULL;
+
+ --estudiantes cuyo nombre es nulo
+ SELECT * FROM STUDENTS WHERE FIRSTNAME IS NULL;
+
+ --edades diferentes de 22
+ SELECT * FROM STUDENTS WHERE AGE NOT IN (22);
